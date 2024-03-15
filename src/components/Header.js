96d1f6b0f53731464f2c386bd278c841ecf6a1b7
Wendy Chen <lfwendy@gmail.com>
2024-03-15 13:50:50 +0800

    newest

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../App.css';
+
+export const Header = () => {
+	return (
+		<h2>
+			Expense Tracker
+		</h2>
+	)
+}
+
+
---
+++ src/context/AppReducer.js
@@ -0,0 +1,20 @@
+// Reducer:
+// How we specify the application's state changes, in response to certain actions to our store, to our context.
+
+export default (state, action) => {
+    switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
+        default:
+            return state;
+    }
+}
+
